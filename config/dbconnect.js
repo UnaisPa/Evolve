814193f426c287979006cd910a0c70aef23f3e21
Unais <135096580+UnaisPa@users.noreply.github.com>
2023-12-07 00:12:22 +0530

    Update dbconnect.js

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 //const uri = 'mongodb://127.0.0.1:27017/e-commerce'
-const uri = process.env.MONGODB_URL
+const uri = const uri = 'mongodb+srv://unaismuhmed712:parammel321@evolvecluster.wxunp9m.mongodb.net/e-commerce';
 const connect = () => {
     mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
