067e18068ff5036d38a5d21148f0c3c470d2eb90
Unais <135096580+UnaisPa@users.noreply.github.com>
2023-12-06 21:24:53 +0530

    Update dbconnect.js

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const uri = 'mongodb://127.0.0.1:27017/e-commerce'
+//const uri = 'mongodb://127.0.0.1:27017/e-commerce'
+const uri = process.env.MONGODB_URL
 const connect = () => {
     mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
